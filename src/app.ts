fdc42c714af11497803a0cb4582ae7894b3eff7d
Doğukan Karabıyık <dokarabiyik2008@gmail.com>
2024-09-13 23:56:41 +0300

    chore: Removed cookie-parser

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,12 @@ import express, {Request, Response} from 'express';
 import logger from 'morgan';
 import indexRouter from './routes/index';
 import userRouter from './routes/users';
-import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
 
 app.use('/', indexRouter);
 app.use('/users', userRouter);
