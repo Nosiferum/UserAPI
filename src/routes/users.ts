fb60dbfdf44ee4a8285c151a8bcb0148be9d5b49
Doğukan Karabıyık <dokarabiyik2008@gmail.com>
2024-09-15 20:13:33 +0300

    feat: Added users route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,52 @@
 import {Router, Request, Response} from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import User from '../models/user';
+import wrapAsync from "../utils/wrapAsync";
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-    res.send('Welcome');
-});
+router.post('/register', wrapAsync(async (req: Request, res: Response) => {
+    const {username, password} = req.body;
+
+    const userExists = await User.findOne({username});
+    if (userExists) {
+        res.status(400).json({message: 'User already exists'});
+        return;
+    }
+
+    const newUser = new User({username, password});
+    await newUser.save();
+
+    const token = jwt.sign({
+        id: newUser._id,
+        username: newUser.username
+    }, process.env.JWT_SECRET || 'secret', {expiresIn: '1h'});
+    res.status(201).json({message: `User created successfully with token: ${token}`});
+}));
+
+
+router.post('/login', wrapAsync(async (req: Request, res: Response) => {
+    const {username, password} = req.body;
+
+    const user = await User.findOne({username});
+    if (!user) {
+        res.status(400).json({message: 'Invalid credentials'});
+        return;
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password as string);
+    if (!isMatch) {
+        res.status(400).json({message: 'Invalid credentials'});
+        return;
+    }
+
+    const token = jwt.sign({
+        id: user._id,
+        username: user.username
+    }, process.env.JWT_SECRET || 'secret', {expiresIn: '1h'});
+
+    res.json({token});
+}));
 
 export default router;
