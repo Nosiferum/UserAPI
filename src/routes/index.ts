c927d9a93793246110200b2715c362c1edfa5a29
Doğukan Karabıyık <dokarabiyik2008@gmail.com>
2024-09-15 20:42:14 +0300

    feat: Added JWT test endpoint to index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,20 @@
-import {Router, Request, Response} from 'express';
+import {Router, Response} from 'express';
+import User from '../models/user';
+import verifyToken from "../utils/verifyToken";
+import wrapAsync from "../utils/wrapAsync";
+import {ExtendedRequest} from "../types/custom";
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-    res.send('Welcome');
-});
+router.get('/', verifyToken, wrapAsync(async (req: ExtendedRequest, res: Response) => {
+    const user = await User.findById(req.userId);
+    if (!user) {
+        res.status(404).json({message: 'User not found'});
+        return;
+    }
+
+    res.status(200).json({message: `You are authorized: ${user.username}`});
+}));
+
 
 export default router;
