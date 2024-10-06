71a79180c868ff6cd0602eebba22312255f2f781
Doğukan Karabıyık <dokarabiyik2008@gmail.com>
2024-10-06 16:23:46 +0300

    feat: Added cors for front-end service

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,19 @@ import express, {Request, Response} from 'express';
 import logger from 'morgan';
 import indexRouter from './routes/index';
 import userRouter from './routes/users';
+import cors from 'cors';
 
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET, PUT, POST, DELETE'],
+    credentials: true
+}));
 
 app.use('/', indexRouter);
 app.use('/users', userRouter);
